161765d28bf4f08eb9cb3c2abcc3649bc7f1c039
Seanvanq <seanwang313@outlook.com>
2020-05-12 10:01:06 +0800

    0512

---
+++ testProj/build/CMakeFiles/testProj.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testProj.dir/main.c.obj"
+  "libtestProj.dll.a"
+  "testProj.exe"
+  "testProj.exe.manifest"
+  "testProj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testProj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

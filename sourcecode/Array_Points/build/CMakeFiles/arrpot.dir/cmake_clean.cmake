4d778c477095c35d8c0363ea22bc7247c80fa615
Seanvanq <seanwang313@outlook.com>
2020-05-12 14:19:55 +0800

    0512

---
+++ sourcecode/Array_Points/build/CMakeFiles/arrpot.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arrpot.dir/main.c.obj"
+  "arrpot.exe"
+  "arrpot.exe.manifest"
+  "arrpot.pdb"
+  "libarrpot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/arrpot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

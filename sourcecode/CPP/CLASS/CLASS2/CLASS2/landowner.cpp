0709c93a1073bd3d32bda2f50f0c2f4e92ccfdd7
SeanWanq <seanwang313@outlook.com>
2020-06-29 13:48:42 +0800

    0629

---
+++ sourcecode/CPP/CLASS/CLASS2/CLASS2/landowner.cpp
@@ -0,0 +1,6 @@
+#include "landowner.h"
+
+LandOwner::LandOwner()
+{
+
+}
---
+++ sourcecode/CPP/CLASS/CLASS2/CLASS2/landowner.h
@@ -0,0 +1,11 @@
+#ifndef LANDOWNER_H
+#define LANDOWNER_H
+
+
+class LandOwner
+{
+public:
+    LandOwner();
+};
+
+#endif // LANDOWNER_H
